0afea4a93e24ec4f96b70180559eded2fe967a9e
Rishita Reddy Chilla <c.rishita.reddy@gmail.com>
2022-01-12 22:49:45 +0530

    single page application1

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -6,9 +6,15 @@ import { DISHES } from '../shared/dishes';
 })
 export class DishService {
 
-  constructor() { }
+ // constructor() { }
 
   getDishes(): Dish[] {
     return DISHES;
+  }
+  getDish(id: string): Dish{
+    return DISHES.filter((dish) => (dish.id === id))[0];
+  }
+  getFeaturedDish(): Dish{
+    return DISHES.filter((dish) => dish.featured)[0];
   }
 }
