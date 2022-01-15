b7d9d5409d68f63facb4c0992afcaf96e8145d7b
Rishita Reddy Chilla <c.rishita.reddy@gmail.com>
2022-01-15 21:18:13 +0530

    Reactive Forms - Feedback form validation

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { Component, OnInit , ViewChild} from '@angular/core';
+import { FormBuilder, FormGroup, Validators, NgForm} from '@angular/forms';
 import { Feedback, ContactType} from '../shared/feedback';
 @Component({
   selector: 'app-contact',
@@ -11,6 +11,7 @@ export class ContactComponent implements OnInit {
   feedbackForm!: FormGroup;
   feedback!: Feedback;//datamodel 
   contactType = ContactType;
+  @ViewChild('fform') feedbackFormDirective!:  NgForm;
   constructor(private fb: FormBuilder) { 
     this.createForm();
   }
@@ -19,10 +20,10 @@ export class ContactComponent implements OnInit {
   }
   createForm(){
     this.feedbackForm = this.fb.group({
-      firstname: '',
-      lastname: '',
-      telnum:0,
-      email:'',
+      firstname: ['',Validators.required],
+      lastname: ['',Validators.required],
+      telnum:[0,Validators.required],
+      email:['',Validators.required],
       agree:false,
       contacttype:'None',
       message:''
@@ -32,7 +33,16 @@ export class ContactComponent implements OnInit {
   onSubmit(){
     this.feedback = this.feedbackForm.value;
     console.log(this.feedback);
-    this.feedbackForm.reset();
+    this.feedbackForm.reset({
+      firstname: '',
+      lastname: '',
+      telnum: 0,
+      email: '',
+      agree: false,
+      contattype:'None',
+      message:''
+    });
+    this.feedbackFormDirective.resetForm();
   }
 
 }
