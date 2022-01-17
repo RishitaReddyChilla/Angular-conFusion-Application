8f562c8dec59d4978f15421e8c666b99d297df5f
Rishita Reddy Chilla <c.rishita.reddy@gmail.com>
2022-01-17 22:08:55 +0530

    RxJS - 1

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DISHES } from '../shared/dishes';
+import { of , lastValueFrom } from 'rxjs';
+import { delay } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
 export class DishService {
 
- // constructor() { }
+  constructor() { }
 
-  getDishes(): Promise<Dish[]>{
+  getDishes(): Promise<Dish[]> {
+    //return of(DISHES).pipe(delay(2000)).lastValueFrom();
+  
     return new Promise(resolve => {
       //without timeout
       //return Promise.resolve(DISHES);
@@ -18,7 +23,16 @@ export class DishService {
     );
   }
   getDish(id: string): Promise<Dish>{
-    //without timeout
+   /* //without timeout
+    //return Promise.resolve(DISHES.filter((dish) => (dish.id === id))[0]);
+    return new Promise(resolve => {
+      //simulate server latency with 2 second delay-short delay
+      setTimeout(()=>resolve(DISHES.filter((dish) => (dish.id === id))[0]),2000)
+    }
+    );
+  }*/
+
+  //without timeout
     //return Promise.resolve(DISHES.filter((dish) => (dish.id === id))[0]);
     return new Promise(resolve => {
       //simulate server latency with 2 second delay-short delay
@@ -27,7 +41,16 @@ export class DishService {
     );
   }
   getFeaturedDish(): Promise<Dish>{
-    //without timeout
+    /*//without timeout
+    //return Promise.resolve(DISHES.filter((dish) => dish.featured)[0]);
+    return new Promise(resolve => {
+      //simulate server latency with 2 second delay-short delay
+      setTimeout(()=>resolve(DISHES.filter((dish) => dish.featured)[0]),2000)
+    }
+    );
+  }*/
+
+   //without timeout
     //return Promise.resolve(DISHES.filter((dish) => dish.featured)[0]);
     return new Promise(resolve => {
       //simulate server latency with 2 second delay-short delay
