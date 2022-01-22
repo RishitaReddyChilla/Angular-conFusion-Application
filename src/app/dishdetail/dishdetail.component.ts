4afb99180383c6f4872bd8af075212b35939967a
Rishita Reddy Chilla <c.rishita.reddy@gmail.com>
2022-01-22 15:45:16 +0530

    dishdetail animations

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -7,6 +7,7 @@ import { DishService } from '../services/dish.service';
 import { switchMap } from 'rxjs/operators';
 import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Comment} from '../shared/comment';
+import { trigger, state, style, animate, transition} from '@angular/animations';
 
 
 //hello
@@ -57,7 +58,20 @@ import { Comment} from '../shared/comment';
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility',[
+      state('show', style({
+        transform: 'scale(1.0)',
+        opacity: 1
+      })),
+      state('hidden',style({
+        transform: 'scale(0.5)',
+        opacity: 0
+      })),
+      transition('*=>*',animate('0.5s ease-in-out'))
+    ])
+  ]
 })
 export class DishdetailComponent implements OnInit {
   //dish = DISH;
@@ -77,6 +91,8 @@ export class DishdetailComponent implements OnInit {
 
   //for saving to server
   dishcopy!:Dish;
+  //animation
+  visibility ='shown';
 
   @ViewChild('cform') commentFormDirective!:  NgForm;
 
@@ -149,9 +165,10 @@ export class DishdetailComponent implements OnInit {
     this.dishService.getDishIds()
     .subscribe((dishIds)=>this.dishIds = dishIds);
 
-   this.route.params.pipe(switchMap((params:Params) => this.dishService.getDish(params['id'])))
+   this.route.params
+    .pipe(switchMap((params:Params) => {this.visibility ='hidden' ; return this.dishService.getDish(params['id']);}))
     .subscribe({next:(dish) => {
-      this.dish = dish; this.dishcopy=dish;this.setPrevNext(dish.id);
+      this.dish = dish; this.dishcopy=dish;this.setPrevNext(dish.id); this.visibility='shown';
     },
     error: errmess => this.errMess = <any>errmess});
   }
